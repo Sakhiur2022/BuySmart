4989cf2075095032a82bb971be29bc9ce20c799b
Sakhiur <sakhiur.rahman@northsouth.edu>
2026-02-10 20:10:22 +0600

    chore(eslint): update ESLint configuration to include React and additional plugins

--- main-app/eslint.config.mjs
+++ main-app/eslint.config.mjs
@@ -1,7 +1,6 @@
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
-import { plugin } from 'postcss';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,7 +13,29 @@ const eslintConfig = [
   {
     ignores: ['**/.next/**', '**/node_modules/**'],
   },
-  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json',
+        },
+      },
+    },
+  },
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'prettier',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/recommended',
+    'plugin:eslint-comments/recommended',
+    'plugin:@tanstack/query/recommended',
+  ),
   {
     files: [
       'app/**/*.{js,jsx,ts,tsx}',
@@ -25,6 +46,8 @@ const eslintConfig = [
       '@typescript-eslint/no-require-imports': 'off', // This allows the use of require() in TypeScript files.
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }], // This allows unused variables if they start with an underscore, which is a common convention for indicating that a variable is intentionally unused.
       'no-unused-vars': 'off', // This is to avoid conflicts with the TypeScript version of the rule.
+      'react/react-in-jsx-scope': 'off', // Next.js uses the new JSX transform.
+      'react-hooks/set-state-in-effect': 'off', // Allow mounted guards for client-only UI.
     },
   },
   {
@@ -32,16 +55,6 @@ const eslintConfig = [
     rules: {
       '@typescript-eslint/no-require-imports': 'off',
     },
-    plugins: [
-      '@typescript-eslint',
-      'prettier',
-      'react',
-      'react-hooks',
-      'jsx-a11y',
-      'import',
-      'eslint-comments',
-      '@tanstack/query',
-    ],
   },
 ];
 
